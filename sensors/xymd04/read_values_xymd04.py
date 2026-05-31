9bf643699b3eedbca3d964304b6dce712de97360
AngeEcam <195105@ecam.be>
2026-05-31 08:39:14 +0200

    Update SLAVE_ID to 2 in read_values_xymd04.py for RS485 Modbus toolkit

--- sensors/xymd04/read_values_xymd04.py
+++ sensors/xymd04/read_values_xymd04.py
@@ -4,7 +4,7 @@ from pymodbus.client import ModbusSerialClient
 logging.disable(logging.CRITICAL)
 
 PORT = "/dev/tty.usbserial-A5069RR4"
-SLAVE_ID = 1
+SLAVE_ID = 2
 
 client = ModbusSerialClient(
     port=PORT,
